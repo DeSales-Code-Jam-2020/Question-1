a387072342ab72de8d5a85aba1c9edc36b247977
Jake Gadaleta <jake.gads@gmail.com>
2020-10-23 09:49:51 -0400

    syncing the template

---
+++ main.py
@@ -0,0 +1,10 @@
+def main(str1: str, str2: str):
+    """Write your code here"""
+
+if __name__ == "__main__":
+    print(
+        main( 
+            input("Enter first word: "),
+            input("Enter second word: ")
+        )
+    )
